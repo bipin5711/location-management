0c449127dd3380ee8e545adeab0cb6009494cc2e
bipin-goodworklabs <bipin.dubey@goodworklabs.com>
2024-03-11 22:29:07 +0530

    try catch added in location controller

--- controllers/location.js
+++ controllers/location.js
@@ -1,56 +1,74 @@
 export const locationData = [];
 
 export const getAllLocations = (req, res) => {
-  res.status(200).send(locationData);
+  try {
+    res.status(200).send(locationData);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const postLocation = (req, res) => {
-  console.log(req.body);
+  try {
+    const { name, lat, lng } = req.body;
 
-  const { name, lat, lng } = req.body;
-
-  if (!name || !lat || !lng) {
-    return res.status(400).send("Please send name, latitude and longitude");
+    if (!name || !lat || !lng) {
+      return res.status(400).send("Please send name, latitude and longitude");
+    }
+    locationData.push({ name, lat, lng, id: locationData.length + 1 });
+    res.status(201).send("Location added successfully!");
+  } catch (error) {
+    console.log(error);
   }
-  locationData.push({ name, lat, lng, id: locationData.length + 1 });
-  res.status(201).send("Location added successfully!");
 };
 
 export const getLocation = (req, res) => {
-  const { id } = req.params;
-  const location = locationData.filter((loc) => loc.id == id);
-  if (!location || location.length == 0) {
-    return res.status(404).send(`Location with id ${id} not found`);
-  }
+  try {
+    const { id } = req.params;
+    const location = locationData.filter((loc) => loc.id == id);
+    if (!location || location.length == 0) {
+      return res.status(404).send(`Location with id ${id} not found`);
+    }
 
-  res.status(200).send(location[0]);
+    res.status(200).send(location[0]);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const updateLocation = (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const { name, lat, lng } = req.body;
-  const locationIndex = locationData.findIndex((loc) => loc.id == id);
-  if (locationIndex == -1) {
-    return res.status(404).send(`Location with id ${id} not found`);
+    const { name, lat, lng } = req.body;
+    const locationIndex = locationData.findIndex((loc) => loc.id == id);
+    if (locationIndex == -1) {
+      return res.status(404).send(`Location with id ${id} not found`);
+    }
+    locationData[locationIndex] = {
+      id,
+      name,
+      lat,
+      lng,
+    };
+
+    res.status(200).send("Location updated successfully!");
+  } catch (error) {
+    console.log(error);
   }
-  locationData[locationIndex] = {
-    id,
-    name,
-    lat,
-    lng,
-  };
-
-  res.status(200).send("Location updated successfully!");
 };
 
 export const deleteLocation = (req, res) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  const locationIndex = locationData.findIndex((loc) => loc.id == id);
-  if (locationIndex == -1) {
-    return res.status(404).send(`Location with id ${id} not found`);
+    const locationIndex = locationData.findIndex((loc) => loc.id == id);
+    if (locationIndex == -1) {
+      return res.status(404).send(`Location with id ${id} not found`);
+    }
+    locationData.splice(locationIndex, 1);
+    res.status(200).send("Location deleted successfully!");
+  } catch (error) {
+    console.log(error);
   }
-  locationData.splice(locationIndex, 1);
-  res.status(200).send("Location deleted successfully!");
 };
